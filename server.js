6a6edb59d66a97ef6c2084873ae2efe9da347292
Sajtoschips <benceboglari@gmail.com>
2023-12-04 14:22:33 +0100

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+require("dotenv").config()  // A .env fájlt olvassa
+const trainings = require("./routes/trainings")
+
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (error) => {
+    console.log(error);
+});
+database.once("connected", () => {
+    console.log(`Database Connected ${database.host}`);
+});
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/trainings", trainings)
+
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
